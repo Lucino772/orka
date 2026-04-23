f4607186a13fd986cb684f05b880609f3bde38e5
Lucino772 <lucapalmi772@gmail.com>
2026-04-23 17:46:03 +0200

    feat: added tanstack query + setup query client

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,11 +2,27 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            staleTime: 30_000,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
+const router = createRouter({
+    routeTree,
+    context: {
+        queryClient,
+    },
+});
 
 declare module "@tanstack/react-router" {
     interface Register {
@@ -19,7 +35,9 @@ if (!rootElement.innerHTML) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
         <StrictMode>
-            <RouterProvider router={router} />
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
         </StrictMode>
     );
 }
