a4a5fc82decbd6350d33d6d05a199f933195c857
Lucino772 <lucapalmi772@gmail.com>
2026-04-23 17:52:27 +0200

    chore: updated ignore rules in eslint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -24,5 +24,11 @@ export default defineConfig([
             globals: globals.browser,
         },
     },
+    {
+        files: ["src/routes/**/*.{ts,tsx}", "src/components/ui/**/*.{ts,tsx}"],
+        rules: {
+            "react-refresh/only-export-components": "off",
+        },
+    },
     eslintConfigPrettier,
 ]);
